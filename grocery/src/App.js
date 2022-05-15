bb3d406d82e397d18c8cc30326a0728c64b1b822
MaheshSrin <smmeka1987@gmail.com>
2022-05-15 11:37:22 +0530

    WIP Product Banner

--- grocery/src/App.js
+++ grocery/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/header';
 import Footer from './Components/Footer';
+import ProductBanner from './Components/ProductBanner';
 function App() {
   return (
     <div>
       <Header></Header>
+      <ProductBanner/>
       <Footer />
     </div>
   );
