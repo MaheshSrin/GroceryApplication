5a5cb8ae797271952a1ff959ee4516cc726a4b02
Prashant Dey <prashantsavior@gmail.com>
2022-05-15 14:26:08 +0530

    WIP: Ratings

---
+++ ratings/ratings1/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Rating from './components/rating';
+
+
+function App() {
+  return (
+    <div className="App">
+     <Rating />
+    </div>
+  );
+}
+
+export default App;
