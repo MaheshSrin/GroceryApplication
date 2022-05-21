36b96cfbceab911e319ace746d2a768363725d65
Balraj Singh Bhullar <bhullar.bs@gmail.com>
2022-05-21 13:33:04 +0530

    WIP:Fix for pagination

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,34 @@
-let startindex = '';
-if (item == 1) {
-  startindex = 0;
-} else {
-  startindex = (item - 1) * 8;
-}
-var express = require('express');
-const groceryData = require('./data.json');
-var app = express();
-let item = 0;
+var express = require("express");
+const groceryData = require("./data.json");
+var app = express()
+let item = 0
 
 app.get('/:items', (req, res) => {
-  let d = [];
-  console.log('--------------------------');
-  res.header('Access-Control-Allow-Origin', '*');
-  item = req.params.items;
-  let startindex = '';
-  if (item == 1) {
-    startindex = 0;
-  } else {
-    startindex = (item - 1) * 8;
-  }
-  console.log(item);
-  for (let i = startindex; i < startindex + 8; i++) {
-    console.log(groceryData[i]);
-    console.log('---------------' + groceryData[i]);
-    if (groceryData[i] == 'undefined') {
-      console.log(groceryData[i]);
-    } else {
-      d.push(groceryData[i]);
+    let d = []
+    console.log("--------------------------")
+    res.header("Access-Control-Allow-Origin", "*");
+    item = req.params.items
+    let startindex = '';
+    if (item == 1){
+        startindex = 0
+    }else{
+        startindex =((item-1)*8)
     }
-  }
-  console.log(d);
-
-  return res.json(d);
+    console.log(item)
+    for (let i = startindex; i<startindex+8; i++){
+        console.log(groceryData[i])
+        console.log("---------------"+groceryData[i])
+        if (groceryData[i] == 'undefined'){
+            console.log(groceryData[i])
+        }
+        else{
+            d.push(groceryData[i])
+        }      
+        
+    }
+    console.log(d)   
+    
+    return res.json(d);
 });
 
-app.listen(3001);
+app.listen(3001)
