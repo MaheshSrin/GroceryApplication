9e5e8bb551ec2ee32e95def6dbe515c2627fe7ba
Shivanshu Pandey <shivanshup1@gmail.com>
2022-05-21 13:29:38 +0530

    WIP: pagination

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,9 @@
-// var express = require("express");
-// const groceryData = require("./data.json");
-// var app = express()
-// const cors = require( 'cors' );
-// app.use( cors() );
-
-// console.log(groceryData)
-// app.get('/', (req, res) => {
-//     return res.json(groceryData);
-// });
-
-// app.listen(3001)
-
+let startindex = '';
+if (item == 1) {
+  startindex = 0;
+} else {
+  startindex = (item - 1) * 8;
+}
 var express = require('express');
 const groceryData = require('./data.json');
 var app = express();
@@ -18,38 +11,26 @@ let item = 0;
 
 app.get('/:items', (req, res) => {
   let d = [];
-//   console.log('--------------------------');
+  console.log('--------------------------');
   res.header('Access-Control-Allow-Origin', '*');
   item = req.params.items;
-//   console.log(item);
-//   for (let i = item; i <= 8 * item; i++) {
-//     if (groceryData[i * item] == 'undefined') {
-//       console.log('*',groceryData[i]);
-//     } else {
-//       d.push(groceryData[i * item]);
-//     }
-//   }
-  
-//   console.log(d);
-
-let startindex = '';
-    if (item == 1){
-        startindex = ((item-1)*8)+1
-    }else{
-        startindex =item
+  let startindex = '';
+  if (item == 1) {
+    startindex = 0;
+  } else {
+    startindex = (item - 1) * 8;
+  }
+  console.log(item);
+  for (let i = startindex; i < startindex + 8; i++) {
+    console.log(groceryData[i]);
+    console.log('---------------' + groceryData[i]);
+    if (groceryData[i] == 'undefined') {
+      console.log(groceryData[i]);
+    } else {
+      d.push(groceryData[i]);
     }
-    item = req.params.items
-    console.log(item)
-    for (let i =startindex; i<startindex+8; i++){
-        if (groceryData[i*item] == 'undefined'){
-            console.log(groceryData[i*item])
-        }
-        else{
-            d.push(groceryData[i*item])
-        }       
-        
-    }
-
+  }
+  console.log(d);
 
   return res.json(d);
 });
