04e5c0515273adc675604c7fcc088ac6ead1e714
aseemgirdhar <aseemgirdhar9@gmail.com>
2022-05-21 12:10:11 +0530

    WIP: fixed card height and content

--- grocery/src/Components/Product/ProductCard.js
+++ grocery/src/Components/Product/ProductCard.js
@@ -1,17 +1,24 @@
+import './productCard.css'
 const ProductCard = (props) => {
   return (
-    <div className="col-lg-3 col-md-4 col-sm-6 mb-3 mt-3">
-      <div class="card">
-        <img class="card-img-top" alt="card" src={props.imgSrc} />
-        <div class="card-body">
-          <strong class="card-title">{props.productTitle}</strong>
-          <p>Price: INR {props.productPrice}</p>
-          <p class="card-text">
-            {props.productDescription}
-          </p>
+
+        <div className="col-lg-3 col-md-4 col-sm-6 mb-3 mt-3 pp">
+          <div class="card">
+              <div className='img-wrapper'>
+                <img class="card-img-top" alt="card" src={props.imgSrc} />
+              </div>
+            <div class="card-body">
+              <strong class="card-title">{props.productTitle}</strong>
+              <p>Price: <strong> INR {props.productPrice}</strong></p>
+              <p class="card-text" title={props.productDescription}>
+                {props.productDescription}
+
+              </p>
+                <button className='btn btn-success btn-sm pull-right'>Read More</button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+
   );
 };
 export default ProductCard
--- grocery/src/Components/Product/ProductRange.js
+++ grocery/src/Components/Product/ProductRange.js
@@ -2,12 +2,14 @@ import ProductCard from "./ProductCard";
 
 const ProductRange = (props) => {
   return (
-    <div className="row">
-        {props.productData.map((item)=><ProductCard key={item.productId} productTitle={item.productTitle} productPrice={item.productPrice} productDescription={item.productDescription} imgSrc={item.imgSrc}></ProductCard>)}
-       <div class="col-12 text-center mb-3">
-       <button type="button" class="btn btn-success">Show More</button>
-       </div>
-    </div>
+      <div className='product-wrapper'>
+        <div className="row">
+                {props.productData.map((item)=><ProductCard key={item.productId} productTitle={item.productTitle} productPrice={item.productPrice} productDescription={item.productDescription} imgSrc={item.imgSrc}></ProductCard>)}
+            <div class="col-12 text-center mb-3">
+           <button type="button" class="btn btn-success">Show More</button>
+           </div>
+        </div>
+      </div>
   );
 };
 export default ProductRange;
