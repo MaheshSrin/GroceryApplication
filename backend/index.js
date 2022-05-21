87bf71ac7d88e144e2d253edbd473d00181fd17d
Shivanshu Pandey <shivanshup1@gmail.com>
2022-05-21 13:15:38 +0530

    WIP: pagination

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,57 @@
-var express = require("express");
-const groceryData = require("./data.json");
-var app = express()
+// var express = require("express");
+// const groceryData = require("./data.json");
+// var app = express()
+// const cors = require( 'cors' );
+// app.use( cors() );
 
-console.log(groceryData)
-app.get('/', (req, res) => {
-    return res.json(groceryData);
+// console.log(groceryData)
+// app.get('/', (req, res) => {
+//     return res.json(groceryData);
+// });
+
+// app.listen(3001)
+
+var express = require('express');
+const groceryData = require('./data.json');
+var app = express();
+let item = 0;
+
+app.get('/:items', (req, res) => {
+  let d = [];
+//   console.log('--------------------------');
+  res.header('Access-Control-Allow-Origin', '*');
+  item = req.params.items;
+//   console.log(item);
+//   for (let i = item; i <= 8 * item; i++) {
+//     if (groceryData[i * item] == 'undefined') {
+//       console.log('*',groceryData[i]);
+//     } else {
+//       d.push(groceryData[i * item]);
+//     }
+//   }
+  
+//   console.log(d);
+
+let startindex = '';
+    if (item == 1){
+        startindex = ((item-1)*8)+1
+    }else{
+        startindex =item
+    }
+    item = req.params.items
+    console.log(item)
+    for (let i =startindex; i<startindex+8; i++){
+        if (groceryData[i*item] == 'undefined'){
+            console.log(groceryData[i*item])
+        }
+        else{
+            d.push(groceryData[i*item])
+        }       
+        
+    }
+
+
+  return res.json(d);
 });
 
-app.listen(3001)
+app.listen(3001);
--- grocery/src/Components/Product/ProductRange.js
+++ grocery/src/Components/Product/ProductRange.js
@@ -1,15 +1,57 @@
-import ProductCard from "./ProductCard";
+import ProductCard from './ProductCard';
+import { Link } from 'react-router-dom';
 
 const ProductRange = (props) => {
+  console.table('props',props.productData)
+  // let pItem = props.productData;
   return (
-      <div className='product-wrapper'>
-        <div className="row">
-                {props.productData.map((item)=><ProductCard key={item.productId} productTitle={item.productTitle} productPrice={item.productPrice} productDescription={item.productDescription} imgSrc={item.imgSrc}></ProductCard>)}
-            <div class="col-12 text-center mb-3">
-           <button type="button" class="btn btn-success">Show More</button>
-           </div>
+    <div className="product-wrapper">
+      <div className="row">
+        {props.productData.map((item) => (
+          <ProductCard
+            key={item.productId}
+            productTitle={item.productTitle}
+            productPrice={item.productPrice}
+            productDescription={item.productDescription}
+            imgSrc={item.imgSrc}
+          ></ProductCard>
+        ))}
+        <div class="col-12 mb-3">
+          {/* <button type="button" class="btn btn-success">Show More</button> */}
+          <nav
+            aria-label="Page navigation example"
+          >
+            <ul class="pagination">
+              <li class="page-item">
+                <Link class="page-link" to="#" aria-label="Previous">
+                  <span aria-hidden="true">&laquo;</span>
+                </Link>
+              </li>
+              <li class="page-item">
+                <Link class="page-link" to="#">
+                  1
+                </Link>
+              </li>
+              <li class="page-item">
+                <Link class="page-link" to="#">
+                  2
+                </Link>
+              </li>
+              <li class="page-item">
+                <Link class="page-link" to="#">
+                  3
+                </Link>
+              </li>
+              <li class="page-item">
+                <Link class="page-link" to="#" aria-label="Next">
+                  <span aria-hidden="true">&raquo;</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
+    </div>
   );
 };
 export default ProductRange;
